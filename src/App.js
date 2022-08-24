50c95930680188c353fe0662024f11094e3fc0b7
bahaa1985 <bahaa_salaheldin@live.com>
2022-08-24 23:02:46 +0200

    sign controls commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SignUP from './signup';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +16,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <div>
+        <SignUP/>
+      </div>
     </div>
   );
 }
--- src/signup.js
+++ src/signup.js
@@ -3,12 +3,14 @@ import react from 'react'
 export default function SignUP(){
     return(
         <div>
-            <input name='comp_name' type='text'/>
-            <input name='comp_pass' type='password'/>
-            <input name='pass_confirm' type='password'/>
-            <select name='comp_type'>
-                <option value='1'></option>
+            <input name='comp_name' type='text' placeholder='اسم الشركة'/>
+            <select name='comp_type' placeholder='اسم الشركة'>
+                <option value='1'>مزود</option>
+                <option value='2'>عميل</option>
             </select>
+            <input name='comp_pass' type='password' placeholder='كلمة المرور'/>
+            <input name='pass_confirm' type='password' placeholder='تأكيد كلمة المرور'/>
+            <input name='comp_tel' type='text' placeholder='رقم الموبايل'/>
         </div>
     )
 }
